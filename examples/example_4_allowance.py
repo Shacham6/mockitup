1b03961186bf1c297b3bee4d6bd561ce7f319c8f
Shacham Ginat <shacham.ginat@pagaya.com>
2021-09-25 16:49:52 +0300

    Add readme and some examples

---
+++ examples/example_4_allowance.py
@@ -0,0 +1,9 @@
+from unittest.mock import Mock
+from mockitup import allow
+
+
+def use_allow_for_simple_cases_example():
+    mock = Mock()
+    allow(mock).get_five().returns(5)
+
+    assert mock.get_five() == 5
--- mockitup/__init__.py
+++ mockitup/__init__.py
@@ -1,1 +1,4 @@
+from . import composer
 from .composer import expectation_suite
+
+allow = composer.ExpectationSuite(False).allow
